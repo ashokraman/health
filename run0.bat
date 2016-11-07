ff27d009e527668ea31907880e24f8d8309c0da3
ashokraman <ashoknraman@gmail.com>
2016-11-07 21:54:18 +0530

    input changes

---
+++ run0.bat
@@ -0,0 +1,1 @@
+python concept_gen.py -i baseline_counselor_in.csv -o bc_concepts -d conceptDictionary.csv 
---
+++ run1.bat
@@ -0,0 +1,1 @@
+python concept_gen.py -i baseline_in.csv -o b_concepts -d conceptDictionary.csv:bc_concepts.csv:bc_concepts_sets.csv 
